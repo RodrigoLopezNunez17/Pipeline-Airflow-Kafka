bd9196e57e3f8bef78810c8847be0b2fbd0db2c7
IMNOTROY <roylopnun@gmail.com>
2025-03-02 00:59:10 -0600

    app updates

--- app.sh
+++ app.sh
@@ -1,5 +1,5 @@
 # Installing the required packages
-sudo apt-get update
+# sudo apt-get update
 
 # sudo apt-get install default-libmysqlclient-dev build-essential
 # pip install mysqlclient
@@ -9,17 +9,10 @@ sudo apt-get update
 
 python MySQL/CreateDB.py
 
-
 # First of all, i need to initialize the airflow server
 
-source venv/bin/activate
 source airflow/airflow-start.sh
 
 # Then i need to initialize the kakfka server so that i need to initialize the zookeeper server first
 
-cd kafka
-bin/zookeeper-server-start.sh config/zookeeper.properties --daemon
-bin/kafka-server-start.sh config/server.properties --deamon
-bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic airflow --from-beginning
-
-# Then i need to initialize the mysql server
+source kafka/kafka-start.sh
