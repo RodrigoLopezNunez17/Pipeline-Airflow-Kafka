197dddefefcfcdbaa9f0d025b8ad68f45bcfb0e4
IMNOTROY <roylopnun@gmail.com>
2025-03-03 14:06:18 -0600

    Web app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
+import subprocess
 
 app = Flask(__name__)
 
@@ -8,7 +9,10 @@ def index():
 
 @app.route('/init', methods=["POST"])
 def init():
-    return "You've init the environment"
+    subprocess.run("source app.sh", shell = True)
+    subprocess.run("sleep 5", shell = True)
+    render_template('initiation.html', methods = ['POST', 'GET'])
+    return redirect('localhost:8080')
 
 if __name__ == "__main__":
     app.run(debug = True)
